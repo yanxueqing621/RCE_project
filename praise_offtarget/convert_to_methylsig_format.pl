cefe36dcc20c5e2bf082e23962108e0be3230446
yanxueqing621 <yanxueqing621@163.com>
2025-04-07 23:36:35 +0800

    Update convert_to_methylsig_format.pl

--- praise_offtarget/convert_to_methylsig_format.pl
+++ praise_offtarget/convert_to_methylsig_format.pl
@@ -1,1 +1,26 @@
+#!/usr/bin/perl
+use IO::All;
+use Modern::Perl;
+use Data::Dumper;
+
+my ($infile, $outfile) = @ARGV;
+my $io_in = io("$infile")->chomp;
+my $io_out = io("$outfile");
+
+$io_in->getline;
+while (defined (my $line = $io_in->getline)) {
+    my ($gid, $pos, $total, $deletion,$ratio ) = split /,/, $line;
+        my ($pos1, $pos2);
+        if ($pos =~/-/) {
+                ($pos1, $pos2) = $pos =~/(\d+)-(\d+)/;
+                $pos2 = $pos2 + 1;
+        } else {
+                $pos1 = $pos;
+                $pos2 = $pos + 1;
+        }
+
+        my $undeletion = $total - $deletion;
+        my $newline = join "\t", $gid, $pos1, $pos2, $ratio, $deletion, $undeletion;
+        $io_out->println($newline);
+}
 
